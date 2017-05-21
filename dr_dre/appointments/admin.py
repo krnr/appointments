fc4a78dd11e93d6b392ceff4889105105f379350
krnr <serova_s@land.ru>
2017-05-21 17:31:52 +0300

    Add AppointmentAdmin

--- dr_dre/appointments/admin.py
+++ dr_dre/appointments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Appointment
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'time_start', 'time_end', 'visitor', 
+                    'non_recurring', 'reason')
+    list_filter = ('date', )
+    list_select_related = ('visitor', )
+    search_fields = ('visitor', 'non_recurring', 'reason')

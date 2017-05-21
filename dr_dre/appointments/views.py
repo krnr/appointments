8989e69d6e8371cb70693594d717ac34f57aac66
krnr <serova_s@land.ru>
2017-05-21 15:02:37 +0300

    Use real date in schedule

--- dr_dre/appointments/views.py
+++ dr_dre/appointments/views.py
@@ -1,6 +1,33 @@
+from datetime import date, timedelta
+
 from django.shortcuts import render
 
 # Create your views here.
 
+
+def get_week(date):
+    """
+    Return the working days of the week containing the given date.
+
+    @returns: iterator (!) of working days
+    """
+    day = date - timedelta(days=date.weekday())
+
+    if date.weekday() > 4:
+        # we vist the site on weekend, go to next week
+        day += timedelta(days=7)
+        
+    for n in range(5):
+        yield day
+        day += timedelta(days=1)
+
+
 def main_view(request):
-	return render(request, 'main.html.j2')
+    today = date.today()
+    return render(
+        request, 
+        'main.html.j2',
+        {
+            'week': list(get_week(today)),
+        }
+    )

580c80c95a8cf0a82d434d32e659d27ec39a0acf
krnr <ultrasnet@mail.ru>
2017-05-22 10:58:20 +0300

    Mark booked frames on creation

--- dr_dre/appointments/viewsets.py
+++ dr_dre/appointments/viewsets.py
@@ -1,8 +1,9 @@
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from .models import Appointment
-from .serializers import AppointmentSerializer
+from .serializers import AppointmentSerializer, FrameSerializer
 
 
 class AppointmentViewSet(mixins.CreateModelMixin, 
@@ -10,3 +11,17 @@ class AppointmentViewSet(mixins.CreateModelMixin,
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     permission_classes = (AllowAny, )
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        # Code inject: we need different format of data to return
+        serializer = FrameSerializer(serializer.instance)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data,
+                        status=status.HTTP_201_CREATED,
+                        headers=headers)
--- dr_dre/static/js/schedule.js
+++ dr_dre/static/js/schedule.js
@@ -1,31 +1,30 @@
 jQuery(document).ready(function($){
 
-var ENDPOINT = '/api/v1/appointment/';
+    var ENDPOINT = '/api/v1/appointment/';
 
-var create_appt = function(data){
-    $.ajax({
-         url: ENDPOINT,
-         type: "POST",
-         data: {
-            "date": data['day'],
-            "time_start": data['hour'] + ":" + data['minutes'],
-            "non_recurring": "Anon",
-            "reason": "I broke a leg"
-         },
-         dataType: "json",
-    }).done(function (data) {
-            console.log("Response " + JSON.stringify(data));
-        }
-    ).error(function (data) {
-            console.log("Response " + JSON.stringify(data));
-        }
-    )
-};
+    var create_appt = function(data){
+        $.ajax({
+            url: ENDPOINT,
+            type: "POST",
+            data: {
+                "date": data['day'],
+                "time_start": data['hour'] + ":" + data['minutes'],
+                "non_recurring": "Anon",
+                "reason": "I broke a leg"
+            },
+            dataType: "json",
+            success: function (data) {
+                book_frame(JSON.stringify(data));
+            },
+            error: function (data) {
+                console.log("Response " + JSON.stringify(data));
+            }
+        })
+    };
 
-$('.frame').click(function(){
-    create_appt(this.dataset);
-});
-
+    $('.frame').click(function(){
+        create_appt(this.dataset);
+    });
 
 });
 
